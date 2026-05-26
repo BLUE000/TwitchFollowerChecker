2cf5f91c61806d5a72e96eb07f227d720b676865
BLUE000 <take.1229.blue@gmail.com>
2026-05-26 21:17:52 +0900

    feat: manage and display version dynamically from CMakeLists.txt

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -1,12 +1,16 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#ifndef APP_VERSION_STRING
+#define APP_VERSION_STRING "1.0.0"
+#endif
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    setWindowTitle("TwitchFollowerChecker - Initializing...");
+    setWindowTitle(QString("TwitchFollowerChecker - Ver %1").arg(APP_VERSION_STRING));
 }
 
 MainWindow::~MainWindow()
